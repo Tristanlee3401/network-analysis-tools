2f5b97fdbe0008f381f45416f97c2b08a647d628
tristanlee3401 <tristanlee3401123@gmail.com>
2026-05-26 23:58:52 +0800

    Create ip-to-bin.c script and update README.md

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Network analysis tools from the command line
 <pre>
 /network-analysis-tools
 ├── README.md
+├── ip-to-bin
+├── ip-to-bin.c
 └── subnet_calculator.py
 
 1 directories, 2 files
---
+++ ip-to-bin.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+
+void print_binary(uint32_t ip)
+{
+	uint32_t host_ip = ntohl(ip);
+	for (int i = 31; i >= 0; i--)
+	{
+		int bit = (host_ip >> i) & 1;
+		printf("%d", bit);
+		if (i % 8 == 0 && i != 0)
+		{
+			printf(".");
+		}
+	}
+	printf("\n");
+}
+
+int main()
+{
+	char ip_str[16]; // 111.111.111.111
+	printf("Enter IP address: \n");
+	if (scanf("%s", ip_str) != 1)
+	{
+		printf("Error reading input. \n");
+		return 1;
+	};
+	struct in_addr ip_addr;
+
+	if (inet_pton(AF_INET, ip_str, &ip_addr) == 1)
+	{
+		printf("IP Address: %s\n", ip_str);
+		printf("Binary: ");
+		print_binary(ip_addr.s_addr);
+	}
+	else
+	{
+		printf("Invalid IP address format.\n");
+	}
+	return 0;
+}
